7d5d0deb6622a864ee89cfc3ef4a69355abfed03
Elliot Zheng <56090975+Elliotto3836@users.noreply.github.com>
2025-04-14 15:20:46 +0000

    Working on decision tree

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,24 +5,51 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
+from sklearn.model_selection import train_test_split # Import train_test_split function
+from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+
 #streamlit run streamlit_app.py --server.enableCORS false --server.enableXsrfProtection false
 
 import mlflow
 import dagshub
 
+import graphviz
+
+from sklearn.tree import export_graphviz
+
+
 
 st.title("Data Science Final")
 
-st.markdown("# :blue[📊 Introduction:]")
-st.write("We will be analyzing the Airplane data, we will be predicting whether or not a customer is a loyal or disloyal customer")
+df_original = pd.read_csv("airline.csv")
 
 
-df = pd.read_csv("airline.csv")
 
-app_mode = st.sidebar.selectbox("Select a page",["Business Case and Data Presentation","Data Visualization","Logistic Regression","Decision Tree","Feature Importance and Driving Variables","Hyperparameter Tuning"])
+# Turns categorical variables into numbers
+#df is the one with numbers, original has the label
+df=df_original.copy()
+label_encoders = {}
+
+for column in df.columns:
+    if df[column].dtype == 'object':
+        le = LabelEncoder()
+        df[column] = le.fit_transform(df[column])
+        label_encoders[column] = le
+
+
+
+app_mode = st.sidebar.selectbox("Select a page",["Business Case and Data Presentation","Data Visualization","Logistic Regression","Decision Tree 🌳","Feature Importance and Driving Variables","Hyperparameter Tuning"])
+
 
 if app_mode == "Business Case and Data Presentation":
-    st.dataframe(df.head(5))
+    st.markdown("# :blue[📊 Introduction:]")
+    st.write("We will be analyzing the Airplane data, we will be predicting whether or not a customer is a loyal or disloyal customer")
+
+    st.dataframe(df_original.head(5))
 
 if app_mode == "Data Visualization":
     st.dataframe(df.head(5))
@@ -30,8 +57,27 @@ if app_mode == "Data Visualization":
 if app_mode == "Logistic Regression":
     st.dataframe(df.head(5))
 
-if app_mode == "Decision Tree":
-    st.dataframe(df.head(5))
+if app_mode == "Decision Tree 🌳":
+    st.markdown("# :blue[📊 Introduction:]")
+    X_tree = df.drop(["satisfaction"],axis=1)
+    y_tree = df["satisfaction"] # Target variable
+
+    X_train_tree, X_test_tree, y_train_tree, y_test_tree = train_test_split(X_tree, y_tree, test_size=0.2, random_state=1)
+    #init
+    clf = DecisionTreeClassifier(max_depth=3)
+    #train
+    clf = clf.fit(X_train_tree,y_train_tree)
+    #predict
+    y_pred_tree = clf.predict(X_test_tree)
+
+    st.write("Accuracy:",metrics.accuracy_score(y_test_tree, y_pred_tree))
+
+    feature_names = X_tree.columns
+    feature_cols = X_test_tree.columns
+    dot_data = export_graphviz(clf, out_file=None,feature_names=feature_cols,class_names=['0','1'],filled=True, rounded=True,special_characters=True)
+    
+    graph = graphviz.Source(dot_data)
+    st.graphviz_chart(graph)
 
 if app_mode == "Feature Importance and Driving Variables":
     st.dataframe(df.head(5))
