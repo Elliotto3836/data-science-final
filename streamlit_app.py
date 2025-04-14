6338ecb8b5db4aeb960dc022f152608dfe8cf4a1
Elliot Zheng <56090975+Elliotto3836@users.noreply.github.com>
2025-04-14 14:48:43 +0000

    Setting up

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import mlflow
+import dagshub
+
+
+st.title("Data Science Final")
+
